47be17d56691631fadca531738c72dde0be6117d
clovisdanielcosta <clovisdanielcosta@hotmail.com>
2020-12-20 15:22:46 -0300

    Updating with repositories of classes

---
+++ exemplo-gradle/src/test/java/br/com/exemplo/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
