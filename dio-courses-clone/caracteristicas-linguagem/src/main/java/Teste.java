8a3331c9a3fe8ebe571469fbebd2718cbd29f066
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2021-02-02 14:20:24 -0300

    Updated in linux

---
+++ dio-courses-clone/caracteristicas-linguagem/src/main/java/Teste.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        System.out.println("A B C D E F".toCharArray());
+        System.out.println("Aula de Java".split(" "));
+        System.out.println("Aula".concat(" de Java"));
+    }
+}
